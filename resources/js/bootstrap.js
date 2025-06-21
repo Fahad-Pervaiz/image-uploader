a7111cee12afd1a9931f3dd780405089100b1248
Fahad Pervez <fahadpervez9476@gmail.com>
2025-06-22 01:56:08 +0300

    Fix HTTPS ERROR

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+
 window.axios = axios;
 
+// Set default headers
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// ✅ Add this to fix HTTPS network errors
+window.axios.defaults.baseURL = 'https://laravel-image-gallery.onrender.com';
+
