174166ddbb560bf840fea40be7c4c616fb80e429
Fahad Pervez <fahadpervez9476@gmail.com>
2025-06-24 19:54:02 +0300

    Allow filesize upto 5mb

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -57,7 +57,7 @@ const submit = () => {
                     v-model="form.email"
                     required
                     autofocus
-                    autocomplete="username"
+                    autocomplete="off"
                 />
 
                 <InputError class="mt-2" :message="form.errors.email" />
